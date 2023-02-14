50d869572baf3101bf197eb64b8c7e36c260e483
Caio Barilli <caiobarilli@gmail.com>
2023-02-14 02:53:05 -0300

    delete old

--- _bash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-container_id=$(docker ps -qf "name=_web")
-
-docker exec -it $container_id /bin/bash
--- _permissions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-container_id=$(docker ps -qf "name=_web")
-
-docker exec -it $container_id /bin/bash -c "
-php artisan storage:link
-chmod o+w ./storage/ -R
-"
